e963947c8b6d5d676e7231d16fe70c15818f838d
funnythingz <funnythingz69+github@gmail.com>
2014-11-24 12:17:28 +0900

    updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'coveralls'
-require 'roronoa'
-
 Coveralls.wear!
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'roronoa'
